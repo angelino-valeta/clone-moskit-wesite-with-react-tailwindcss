85d764b0939b6d5e8b61a9e55e52495c6e533ca7
Angelino Valeta <angelinovaleta@gmail.com>
2022-10-11 22:53:20 +0100

    Component Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,163 @@
+import gptw from '../../img/certified.svg';
+
+function Footer(){
+    return(
+        <div className="bg-indigo-200">
+            <div className="px-5 space-y-9 py-10 sm:py-5 md:px-20">
+            <div className="flex flex-col space-y-4 sm:flex-row sm:gap-x-16 sm:items-baseline md:grid md:grid-cols-4">
+               <div className="space-y-1">
+               <h1 className="font-semibold text-lg">Moskit CRM</h1>
+                <ul className="flex flex-col space-y-2 sm:space-y-4 text-sm font-medium md:text-medium">
+                    <li>
+                        <a className="hover:underline" href="#">Programa de Afiliados</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Agendar reunião comercial</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Central de ajuda do CRM</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Aplicativo iOS</a>
+                        &nbsp; &nbsp;| &nbsp; &nbsp; 
+                        <a className="hover:underline" href="#">Android</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Documentação da API</a>
+                    </li>
+                </ul>
+               </div>
+               <div className="space-y-1">
+               <h1 className="font-semibold text-lg">Materiais</h1>
+                <ul className="flex flex-col space-y-2 text-sm font-medium sm:space-y-4 md:text-medium">
+                    <li>
+                        <a className="hover:underline" href="#">Ebooks</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Programas do YouTube</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Webinars</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Cursos</a>
+                    </li>
+                </ul>
+               </div>
+               <div className="space-y-1">
+               <h1 className="font-semibold text-lg">Empresa</h1>
+                <ul className="flex flex-col space-y-2 text-sm font-medium sm:space-y-4 md:text-medium">
+                    <li>
+                        <a className="hover:underline" href="#">Entre em contato</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Conheça nossa equipe</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Trabalhe conosco</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Termos de uso</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Política de privacidade</a>
+                    </li>
+                </ul>
+               </div>
+               <div className="space-y-1">
+               <h1 className="font-semibold text-lg">Site</h1>
+                <ul className="flex flex-col space-y-2 text-sm font-medium sm:space-y-4 md:text-medium">
+                    <li>
+                        <a className="hover:underline" href="#">Clientes</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Planos</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Recursos</a>
+                    </li>
+                    <li> 
+                        <a className="hover:underline" href="#">Blog</a>
+                    </li>
+                </ul>
+               </div>
+            </div>
+            <div className="hidden sm:block">
+                <h2 className="text-2xl">Nosso blog sobre vendas</h2>
+            </div>
+            <div className="flex flex-col space-y-4 sm:flex-row sm:gap-x-5 sm:items-baseline md:gap-x-16">
+               <div className="space-y-1 ">
+               <h1 className="font-semibold text-lg">Conceitos de vendas</h1>
+                <ul className="flex flex-col space-y-2 text-sm font-medium md:text-medium">
+                    <li>
+                        <a className="hover:underline" href="#">Spin Selling</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Inbound e Outbound</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Digital Influencer</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">O que  é CRM</a>
+                    </li>
+                </ul>
+               </div>
+               <div className="space-y-1">
+               <h1 className="font-semibold text-lg">Como fazer</h1>
+                <ul className="flex flex-col space-y-2 text-sm font-medium md:text-medium">
+                    <li>
+                        <a className="hover:underline" href="#">Como ser um bom gestor</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Carta de apresentação comercial</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Como vender pelo whatsapp</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Como prospectar clientes</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Cliente confiança</a>
+                    </li>
+                </ul>
+               </div>
+               <div className="space-y-1">
+               <h1 className="font-semibold text-lg">Modelos</h1>
+                <ul className="flex flex-col space-y-2 text-sm font-medium md:text-medium">
+                    <li>
+                        <a className="hover:underline" href="#">Mensagens para clientes por Whatsapp</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Modelos de e-mail</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Mensagem de ausência para clientes</a>
+                    </li>
+                    <li>
+                        <a className="hover:underline" href="#">Mensagem automática para clientes</a>
+                    </li>
+                </ul>
+               </div>         
+            </div>
+            <div className="flex flex-row items-center">
+                <div className="flex flex-1 space-x-8">
+                    <svg className="w-8 h-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                    <svg className="w-8 h-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                    </svg>
+                </div>
+                <div>
+                    <img src={gptw} alt="GPTW Certification" />
+                </div>
+            </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
