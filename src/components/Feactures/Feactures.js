884298af9258a181bf096ad84ba3893e5fd12846
Angelino Valeta <angelinovaleta@gmail.com>
2022-10-11 22:52:55 +0100

    Component Feactures

---
+++ src/components/Feactures/Feactures.js
@@ -0,0 +1,169 @@
+
+function Feactures(){
+    return(
+        <div className="bg-black pb-10 mb-10 md:mb-32">
+            <div className="text-center">
+                <div className="p-14 mx-auto sm:p-36 relative">
+                    <div className="inline">
+                        <div className="hidden sm:block">
+                            <svg className="text-white w-9 h-auto inline absolute top-24 left-60 animate-bounce" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 13v-1m4 1v-3m4 3V8M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
+                            </svg>
+                            <svg className="text-white w-9 h-auto inline absolute top-24 right-640" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                            </svg>
+                            <svg className="text-white w-9 h-auto inline absolute top-16 right-60 transition animate-pulse" xmlns="http://www.w3.org/2000/svg b" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+                            </svg>
+                            <svg className="text-white w-9 h-auto inline absolute bottom-2 left-20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                            </svg>
+                            <svg className="text-white w-9 h-auto inline absolute bottom-24 left-32" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
+                            </svg>
+                            <svg className="text-white w-9 h-auto inline absolute top-20 left-20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            </svg>
+                            <svg className="text-white w-9 h-auto inline absolute bottom-10 right-72 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
+                            </svg>
+                            <svg className="text-white w-9 h-auto inline absolute bottom-20 right-32" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z" />
+                            </svg>
+                            <svg className="text-white w-9 h-auto inline absolute top-20 right-20" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
+                            </svg>
+                        </div>
+                    </div>
+                    <h1 className="text-2xl sm:text-5xl font-semibold text-white lg:max-w-lg md:mx-auto">Seu CRM conectado ao Google Agenda</h1>
+                </div>
+                <div className="px-3 sm:px-6 lg:px-16 lg:-mb-20">
+                    <div className="bg-white rounded-lg shadow-md px-5 pt-5 pb-9 overflow-hidden lg:py-12 lg:px-16">
+                        <div className="grid grid-row-2 gap-x-3 gap-y-4 sm:grid-cols-3 sm:gap-x-4 s">
+                            <div className="rounded-xl shadow-lg px-2 py-8 sm:px-4">
+                                <div className="space-y-2">
+                                    <div className="flex flex-row space-x-2 items-center">
+                                        <div className="rounded-lg bg-purple-700 p-2">
+                                            <svg className="w-8 h-auto text-white rotate-4 sm:w-12" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+                                            </svg>
+                                        </div>
+                                        <div className="flex flex-col">
+                                            <h3 className="font-semibold text-sm text-left sm:text-md">Gestão de projectos</h3>
+                                            <div className="flex justify-end w-8 h-4 bg-blue-300 rounded-full">
+                                                <div className="w-4 h-4 cursor-pointer bg-blue-600 rounded-full"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p className="text-xs text-left sm:text-sm">Gerencire seus projectos dentro do Moskit</p>
+                                </div>
+                            </div>
+                            <div className="rounded-xl shadow-lg px-2 py-8 sm:px-4">
+                            <div className="space-y-2">
+                                <div className="flex flex-row space-x-2 items-center">
+                                    <div className="rounded-lg bg-pink-500 p-2">
+                                        <svg className="w-8 h-auto text-white rotate-4 sm:w-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M4 3a2 2 0 100 4h12a2 2 0 100-4H4z" />
+                                        <path fill-rule="evenodd" d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <h3 className="font-semibold text-sm text-left sm:text-md">Módulo de Produtos</h3>
+                                        <div className="flex justify-end w-8 h-4 bg-blue-300 rounded-full">
+                                            <div className="w-4 h-4 bg-blue-600 rounded-full cursor-pointer"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p className="text-xs text-left sm:text-sm">Adicione produtos e descontos em suas negociações</p>
+                            </div>
+                        </div>
+                        <div className="rounded-xl shadow-lg px-2 py-8 sm:px-4">
+                            <div className="space-y-2">
+                                <div className="flex flex-row space-x-2 items-center">
+                                    <div className="rounded-lg bg-blue-700 p-2">
+                                        <svg className="w-8 h-auto text-white rotate-4 sm:w-12" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                        </svg>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <h3 className="font-semibold text-sm text-left sm:text-md">Geração de Propostas</h3>
+                                        <div className="flex justify-end w-8 h-4 bg-blue-300 rounded-full">
+                                            <div className="w-4 h-4 bg-blue-600 rounded-full cursor-pointer"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p className="text-xs text-left sm:text-sm">
+                                    Crie propostas comerciais através do Moskit
+                                </p>
+                            </div>
+                        </div>
+                        <div className="rounded-xl shadow-lg px-2 py-8 sm:px-4">
+                            <div className="space-y-2">
+                                <div className="flex flex-row space-x-2 items-center">
+                                    <div className="rounded-lg bg-green-500 p-2">
+                                        <svg className="w-8 h-auto text-white rotate-4 sm:w-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                                        </svg>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <h3 className="font-semibold text-sm text-left sm:text-md">Ligações</h3>
+                                        <div className="flex justify-end w-8 h-4 bg-blue-300 rounded-full">
+                                            <div className="w-4 h-4 bg-blue-600 rounded-full cursor-pointer"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p className="text-xs text-left sm:text-sm">Faça e grave ligações no Moskit utilizando Zenvia</p>
+                            </div>
+                        </div>
+                        <div className="rounded-xl shadow-lg px-2 py-8 sm:px-4">
+                            <div className="space-y-2">
+                                <div className="flex flex-row space-x-2 items-center">
+                                    <div className="rounded-lg bg-blue-400 p-2">
+                                        <svg className="w-8 h-auto text-white rotate-4 sm:w-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+                                        </svg>
+                                    </div>
+                                    <div className="flex flex-col overflow-hidden sm:over">
+                                        <h3 className="font-semibold text-sm text-left sm:text-md">Moskit Videoconferência</h3>
+                                        <div className="flex justify-end w-8 h-4 bg-blue-300 rounded-full">
+                                            <div className="w-4 h-4 bg-blue-600 rounded-full cursor-pointer"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p className="text-xs text-left sm:text-sm">Inicie videoconferências com seu cliente em 2 cliques</p>
+                            </div>
+                        </div>
+                        <div className="rounded-xl shadow-lg px-2 py-8 sm:px-4">
+                            <div className="space-y-2">
+                                <div className="flex flex-row space-x-2 items-center">
+                                    <div className="rounded-lg bg-blue-600 p-2">
+                                        <svg className="w-8 h-auto text-white rotate-4 sm:w-12" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+                                        </svg>
+                                    </div>
+                                    <div className="flex flex-col">
+                                        <h3 className="font-semibold text-sm text-left sm:text-md">API Pública disponível</h3>
+                                        <div className="flex justify-end w-8 h-4 bg-blue-300 rounded-full">
+                                            <div className="w-4 h-4 bg-blue-600 rounded-full cursor-pointer"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p className="text-xs text-left sm:text-sm">API pública para integrações com sistemas externos</p>
+                            </div>
+                        </div>
+                        </div>
+
+                        <div className="mt-5 space-y-6 sm:mt-10 sm:px-16">
+                            <p className="text-sm font-semibold sm:text-lg ">Utilize os módulos e integrações disponíveis no marketplace do Moskit para turbinar seu CRM, personalizando de acordo com suas principais necessidades.
+                            </p>
+                            <button className="bg-blue-600 text-white px-12 py-3 rounded-full font-semibold text-lg transform hover:translate-x-0.5 hover:translate-y-0.5 transition-transform duration-400 shadow-md">Teste um CRM completo</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Feactures;
